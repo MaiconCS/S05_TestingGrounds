0e04ab8206de0d4ddaba2c6e7e72a1b09c665048
Maicon C. Santana <31459524+MaiconCS@users.noreply.github.com>
2020-10-14 18:24:37 -0300

    TG18 Reading Blackboard Data in C++

--- TestingGrounds/Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
+++ TestingGrounds/Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,13 +1,16 @@
 // AbraceTI
 
 
-#include "ChooseNextWaypoint.h"
-
+#include "H:\repos\Unreal\S05_TestingGrounds\TestingGrounds\Source\S05_TestingGrounds\ChooseNextWaypoint.h"
+#include "C:\Program Files\Epic Games\UE_4.22\Engine\Source\Runtime\AIModule\Classes\BehaviorTree\BlackboardComponent.h"
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT(" AI in C++ ! ") );
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+
+	UE_LOG(LogTemp, Warning, TEXT(" Waypoint index: %i "), Index);
 	return EBTNodeResult::Succeeded;
 
 }
--- TestingGrounds/Source/S05_TestingGrounds/ChooseNextWaypoint.h
+++ TestingGrounds/Source/S05_TestingGrounds/ChooseNextWaypoint.h
@@ -2,8 +2,8 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
-#include "BehaviorTree/BTTaskNode.h"
+#include "C:\Program Files\Epic Games\UE_4.22\Engine\Source\Runtime\Core\Public\CoreMinimal.h"
+#include "C:\Program Files\Epic Games\UE_4.22\Engine\Source\Runtime\AIModule\Classes\BehaviorTree\BTTaskNode.h"
 #include "ChooseNextWaypoint.generated.h"
 
 /**
@@ -16,4 +16,14 @@ class S05_TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
+protected:
+	//UPROPERTY ( EditAnywhere ) use to expose variable public in details panel on behaviour tree. 
+	//This is an option on the panel detail in behaviour tree, IndexKey is a list.
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
+	
+
+	
+
+
 };
